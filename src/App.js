cb4af133ff41ce75f3bc1bcecfb24d4c48fd55ca
FinnCummins <finnkcummins@gmail.com>
2023-05-11 16:38:16 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -22,4 +23,48 @@ function App() {
   );
 }
 
-export default App;
+class MarkDownPreviewer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input:""
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+  
+  handleChange(event) {
+    this.setState({
+      input: event.target.value
+    });
+  }
+  
+  render() {
+    return (
+      <div id="markdown-previewer">
+        <div id="textarea-container">
+          <textheader id="textareabar"><text id="bar-label">Text editor</text></textheader>
+          <textarea id="editor" onChange={this.handleChange} value={this.state.input}></textarea>
+        </div>
+        <div id="markdown-container">
+          <textheader id="markdown-bar"><text id="bar-label">Markdown previewer</text></textheader>
+          <div id="preview" class="scrollable-div">{this.state.input}</div>
+        </div>
+        <p>Created by <a href="https://linkedin.com/in/finn-cummins" target="_blank" class="linkedin">Finn Cummins</a></p>
+      </div>
+    )
+  }
+}
+
+export default MarkDownPreviewer;
+//ReactDOM.render(<MarkDownPreviewer/>, document.getElementById("previewer"));
+
+/*const marked = require('marked');
+
+let markdownText = `
+# Hello, world!
+This is a paragraph in **markdown**!
+`;
+
+let htmlText = marked(markdownText);*/
+
+console.log("hello World");
--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import MarkDownPreviewer from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('editor'));
 root.render(
   <React.StrictMode>
-    <App />
+    <MarkDownPreviewer />
   </React.StrictMode>
 );
 
